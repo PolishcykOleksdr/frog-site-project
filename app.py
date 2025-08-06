f7cc5e044776757803e571021271d609d96df1d9
Inferno79402 <sashap07072010@gmail.com>
2025-08-06 19:38:00 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html", basicPrice=0, premiumPrice=9.9, improvInfo = 92,
+                           peopleInfo = 12, continueInfo = 87)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
